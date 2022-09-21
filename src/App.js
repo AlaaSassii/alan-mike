1eda1946c1842aa67f78ab0028dfb8c9d72de992
alaasassii <alaasassijs@gmail.com>
2022-09-21 22:24:09 +0100

    adding things

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React , {useState , useEffect} from 'react' ; 
 import './App.css';
-
+import alanBtn from '@alan-ai/alan-sdk-web';
 function App() {
+  const [cart , setCart] = useState([]) ;  
+  const [menuItems , setMenuItems] = useState([]) ; 
+  useEffect(() => {
+    alanBtn({
+        key: '0370a264f7b1bef476d2c3bfe46df9342e956eca572e1d8b807a3e2338fdd0dc/stage',
+        onCommand: (commandData) => {
+          setMenuItems(commandData.data) 
+        }
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        menuItems.map(item => {
+          const {name , price , category} = item ; 
+          return (
+            <li>
+              {name} - {category} - {price}$
+            </li>
+          )
+        })
+      }
+      <h1>Cart</h1>
+      {
+        cart.map(cartItem => {
+          const {name , price , category} = cartItem ; 
+          return (
+            <li>
+              {name} - {category} - {price}$
+            </li>
+          )
+        })
+      }
     </div>
   );
 }
