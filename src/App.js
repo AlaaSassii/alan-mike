3397a09e6bd88356a97a7e3ddaa4090f84659df9
alaasassii <alaasassijs@gmail.com>
2022-09-22 16:24:06 +0100

    adding

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
     alanBtn({
         key: '0370a264f7b1bef476d2c3bfe46df9342e956eca572e1d8b807a3e2338fdd0dc/stage',
         onCommand: (commandData) => {
+          if(commandData.command === 'show menu')
           setMenuItems(commandData.data) 
+        
         }
     });
   }, []);
@@ -20,6 +22,7 @@ function App() {
           return (
             <li>
               {name} - {category} - {price}$
+              <button>add to cart</button>
             </li>
           )
         })
